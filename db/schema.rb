e47679db7dc9296af3306cf00026b344146edf50
Carter Boyle <carterboyle@msn.com>
2025-07-25 16:34:11 -0300

    Implementing categories and unit testing the category feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_25_164235) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_25_184513) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|

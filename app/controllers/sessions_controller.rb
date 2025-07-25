64f851dcc5850fe0b7bb3ab3e0bac0865aff0bc8
Carter Boyle <carterboyle@msn.com>
2025-07-25 10:28:24 -0300

    Implemented users' ability to login and logout of application
    Next step is to make some authication helper methods

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:notice] = "Logged in succesfully"
+      redirect_to user
+    else
+      # if we used simply flash[:alert] it would persist for another http
+      # request, which is not what we want
+      flash.now[:alert] = "There was something wrong with your login details"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
+  end
+end
